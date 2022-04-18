99d74f639c49bf1d54ece395b1f29376d8bb9c82
damn <alexei.gordienko@hotmail.com>
2022-04-19 01:00:05 +0300

    feat: added functionality

--- photo/photo/urls.py
+++ photo/photo/urls.py
@@ -21,4 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('module1.urls')),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
